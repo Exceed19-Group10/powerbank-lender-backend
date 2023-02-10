678426f72a0c37881c027b1f0f0391c7430d3dfd
ThanadolU <dolthanadolu@gmail.com>
2023-02-10 15:19:03 +0700

    finished

--- database/database.py
+++ database/database.py
@@ -1,11 +1,13 @@
 from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
-load_dotenv('../.env') # load env file
+load_dotenv('.env') # load env file
 
 
 user = os.getenv('user')
+# print(user)
 password = os.getenv('password')
+# print(password)
 client = MongoClient(f'mongodb://{user}:{password}@mongo.exceed19.online:8443/?authMechanism=DEFAULT')
 db = client['exceed10']
 user_database = db['user']
--- router/powerbank.py
+++ router/powerbank.py
@@ -2,7 +2,7 @@ from typing import Union
 from fastapi import APIRouter
 from fastapi.exceptions import HTTPException
 from pydantic import BaseModel
-from datetime import datetime
+from datetime import datetime, timedelta
 from database.database import user_database, powerbank_database
 
 
@@ -32,20 +32,43 @@ class PowerBank(BaseModel):
     end_time: Union[None, datetime]
     late_mai: Union[None, int]
 
+class BorrowLaewNaRequestBody(BaseModel):
+    user_ID: int
+    password: str
+
 
 @router.get('/all-powerbank')
 def get_all_powerbank():
-    pass
+    powerbank_list = list(powerbank_database.find({}, {"_id":False}))
+    return {"all_powerbank": powerbank_list}
 
 
-@router.put('/borrow-laew')
-def borrow_laew_naaaa(user_ID: int, password: str, powerbank_ID: int):
-    pass
+@router.get('/get-powerbank/{powerbank_ID}')
+def get_powerbank(powerbank_ID: int):
+    return list(powerbank_database.find({"powerbank_ID": powerbank_ID}, {'_id': False}))[0]
 
 
-@router.put('/all-status')
-def all_powerbank_status():
-    pass
+@router.put('/borrow-laew/{powerbank_ID}')
+def borrow_laew_naaaa(powerbank_ID: int, body: BorrowLaewNaRequestBody):
+    powerbank = list(powerbank_database.find({"powerbank_ID": powerbank_ID}, {'_id': False}))
+    something = powerbank.pop(0)
+    all_users = list(user_database.find({"user_ID": body.user_ID, "password": body.password}, {'_id': False}))
+    if not len(all_users):
+        raise HTTPException(401, "UserID and Password doesn't match.")
+    user = all_users.pop(0)
+    powerbank_database.update_one(something, {"$set": 
+                                                {
+                                                    "borrow_mai": 1,
+                                                    "yu_mai": 0,
+                                                    "user_ID": body.user_ID,
+                                                    "username": user["username"],
+                                                    "user_dept": user["user_dept"],
+                                                    "start_time": datetime.now().timestamp(),
+                                                    "end_time": (datetime.now() + timedelta(hours=5)).timestamp()
+                                                }
+                                            }
+                                        )
+    return something
 
 
 @router.put('/return-laew')
