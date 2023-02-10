6c8cd145605ed418669a4941316b648c7ce71c0d
get <run001na@gmail.com>
2023-02-10 15:12:36 +0700

    update1

--- database/database.py
+++ database/database.py
@@ -1,12 +1,13 @@
 from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
-load_dotenv('../.env') # load env file
+load_dotenv('.env') # load env file
 
 
 user = os.getenv('user')
 password = os.getenv('password')
 client = MongoClient(f'mongodb://{user}:{password}@mongo.exceed19.online:8443/?authMechanism=DEFAULT')
+# client = MongoClient(f'mongodb://exceed10:SR5RAqhy@mongo.exceed19.online:8443/?authMechanism=DEFAULT')
 db = client['exceed10']
-user_database = db['user']
-powerbank_database = db['powerbank']
+user_database = db['user2']
+powerbank_database = db['powerbank2']
--- router/powerbank.py
+++ router/powerbank.py
@@ -5,13 +5,11 @@ from pydantic import BaseModel
 from datetime import datetime
 from database.database import user_database, powerbank_database
 
-
 router = APIRouter(
-        prefix="/powerbank",
-        tags=["powerbank"],
+        prefix="/powerbank2",
+        tags=["powerbank2"],
         responses={404: {"description": "Not found"}},
     )
-
 
 class User(BaseModel):
     user_ID: int
@@ -19,7 +17,6 @@ class User(BaseModel):
     username: str
     user_dept: str
     user_fee: Union[None, int]
-
 
 class PowerBank(BaseModel):
     powerbank_ID: int
@@ -32,27 +29,53 @@ class PowerBank(BaseModel):
     end_time: Union[None, datetime]
     late_mai: Union[None, int]
 
-
 @router.get('/all-powerbank')
 def get_all_powerbank():
-    pass
+    return {"all_powerbank": list(powerbank_database.find({}, {"_id":False}))}
 
-
-@router.put('/borrow-laew')
+@router.put('/borrow-laew/{user_ID}/{password}/{powerbank_ID}')
 def borrow_laew_naaaa(user_ID: int, password: str, powerbank_ID: int):
-    pass
+    filter_powerbank = list(powerbank_database.find({"powerbank_ID": powerbank_ID}, {"_id":False}))
+    powerbank = filter_powerbank.pop(0)
+    filter_user = list(user_database.find({"user_ID": user_ID}, {"_id":False}))
+    user = filter_user.pop(0)
+    if user_ID!=user["user_ID"]:
+        return HTTPException(404, "The username is wrong.")
+    if password!=user["password"]:
+        return HTTPException(404, "The password is wrong")
+    if powerbank["borrow_mai"]==0:
+        powerbank_database.update_one({"powerbank_ID": powerbank_ID},{"$set":{"borrow_mai":1}})
+        powerbank_database.update_one({"powerbank_ID": powerbank_ID},{"$set":{"user_ID":user_ID}})
+        powerbank_database.update_one({"powerbank_ID": powerbank_ID},{"$set":{"yu_mai":0}})
+        return powerbank
+    return HTTPException(400, "This powerbank is currently being borrow.")
 
 
 @router.put('/all-status')
 def all_powerbank_status():
     pass
 
-
-@router.put('/return-laew')
+@router.get('/return-laew/{powerbank_ID}')
 def return_powerbank(powerbank_ID: int):
-    pass
+    filter_powerbank = list(powerbank_database.find({"powerbank_ID": powerbank_ID}, {"_id":False}))
+    powerbank = filter_powerbank.pop(0)
+    if powerbank["borrow_mai"]==1:
+        powerbank_database.update_one({"powerbank_ID": powerbank_ID},{"$set":{"borrow_mai":0}})
+        powerbank_database.update_one({"powerbank_ID": powerbank_ID},{"$set":{"user_ID":0}})
+        powerbank_database.update_one({"powerbank_ID": powerbank_ID},{"$set":{"yu_mai":1}})
+        powerbank_database.update_one({"powerbank_ID": powerbank_ID},{"$set":{"username":""}})
+        powerbank_database.update_one({"powerbank_ID": powerbank_ID},{"$set":{"user_dept":""}})
+        powerbank_database.update_one({"powerbank_ID": powerbank_ID},{"$set":{"start_time":0}})
+        powerbank_database.update_one({"powerbank_ID": powerbank_ID},{"$set":{"end_time":0}})
+        powerbank_database.update_one({"powerbank_ID": powerbank_ID},{"$set":{"late_mai":0}})
+        return powerbank
+    return HTTPException(400, "This powerbank is not borrowed.")
 
-
-@router.put('/pai-laew')
+@router.get('/pai-laew/{powerbank_ID}')
 def pai_leaw_naaaa(powerbank_ID: int):
-    pass
+    filter_powerbank = list(powerbank_database.find({"powerbank_ID": powerbank_ID}, {"_id":False}))
+    powerbank = filter_powerbank.pop(0)
+    if powerbank["yu_mai"]==powerbank["borrow_mai"]:
+        powerbank_database.update_one({"powerbank_ID": powerbank_ID},{"$set":{"yu_mai":0}})
+        return powerbank
+    return HTTPException(400, "The powerbank is still here.")
