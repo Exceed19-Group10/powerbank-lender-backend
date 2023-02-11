d77eeb385a3d4a00060b18e706e0925bda958a1c
ThanadolU <dolthanadolu@gmail.com>
2023-02-11 22:26:09 +0700

    fixed condition in confirm return

--- router/powerbank.py
+++ router/powerbank.py
@@ -49,26 +49,32 @@ class BorrowLaewNaRequestBody(BaseModel):
 
 @router.get('/all-powerbank')
 def get_all_powerbank():
+    """Get all powerbank data from powerbank collection."""
     powerbank_list = list(powerbank_database.find({}, {"_id":False}))
     return {"all_powerbank": powerbank_list}
 
 
 @router.get('/get-powerbank/{powerbank_ID}')
 def get_powerbank(powerbank_ID: int):
+    """Get specific powerbank data by powerbank_ID from powerbank collection."""
     return list(powerbank_database.find({"powerbank_ID": powerbank_ID}, {'_id': False}))[0]
 
 
 @router.put('/borrow-laew/{powerbank_ID}')
 def borrow_laew_naaaa(powerbank_ID: int, body: BorrowLaewNaRequestBody):
+    """"""
     powerbank = list(powerbank_database.find({"powerbank_ID": powerbank_ID}, {'_id': False}))
-    something = powerbank.pop(0)
+    try:
+        something = powerbank.pop(0)
+    except IndexError:
+        raise HTTPException()
     # print(body)
     from_user_database = list(user_database.find({"user_ID": body.user_ID, "password": body.password}, {'_id': False}))
     if not len(from_user_database):
         raise HTTPException(401, "Authentication Error because UserID and Password doesn't match.")
     user = from_user_database.pop(0)
     if user["user_fee"] != 0:
-        raise HTTPException(406, "You haven't paid your fee.")
+        raise HTTPException(402, "You haven't paid your fee.")
     powerbank_database.update_one(something, {"$set": 
                                                 {   
                                                     "borrow_mai": 1,
@@ -91,7 +97,8 @@ def borrow_laew_naaaa(powerbank_ID: int, body: BorrowLaewNaRequestBody):
             "borrow_time": updated_powerbank["end_time"] - updated_powerbank["start_time"], 
             "late_mai": 0,
             "time": updated_powerbank["start_time"]
-        })
+        }
+    )
     return {   
                 "powerbank_ID": powerbank_ID,
                 "borrow_mai": 1,
@@ -106,6 +113,7 @@ def borrow_laew_naaaa(powerbank_ID: int, body: BorrowLaewNaRequestBody):
 
 @router.put('/return-laew/{powerbank_ID}')
 def return_powerbank(powerbank_ID: int):
+    """This method is used when powerbank is back to its position."""
     powerbank = list(powerbank_database.find({"powerbank_ID": powerbank_ID}, {'_id': False}))
     try:
         something = powerbank.pop(0)
@@ -117,6 +125,7 @@ def return_powerbank(powerbank_ID: int):
 
 @router.put('/pai-laew/{powerbank_ID}')
 def pai_leaw_naaaa(powerbank_ID: int):
+    """This method is used when powerbank is left from its position."""
     powerbank = list(powerbank_database.find({"powerbank_ID": powerbank_ID}, {'_id': False}))
     try:
         something = powerbank.pop(0)
@@ -128,6 +137,7 @@ def pai_leaw_naaaa(powerbank_ID: int):
 
 @router.put('/check-dai-mai/{powerbank_ID}')
 def confirm_return(powerbank_ID: int):
+    """"""
     powerbank = list(powerbank_database.find({"powerbank_ID": powerbank_ID}, {"_id": False}))
     # print(powerbank)
     try:
@@ -138,7 +148,7 @@ def confirm_return(powerbank_ID: int):
     # print(user_history)
     user = user_history.pop(0)
     if something["yu_mai"] == 1:
-        if something["end_time"] < datetime.now().timestamp():
+        if something["end_time"] < (datetime.now() - timedelta(hours=7)).timestamp():
             borrower_history.update_one(user, {"$set": {"late_mai": 1}})
         else:
             borrower_history.update_one(user, {"$set": {"late_mai": 0}})
@@ -158,6 +168,7 @@ def confirm_return(powerbank_ID: int):
 
 @router.get('/fee/{powerbank_ID}')
 def fee(powerbank_ID: int):
+    """Calculate fee when end time is greater than 30 seconds(demo) otherwise, it will be zero."""
     powerbank = list(powerbank_database.find({"powerbank_ID": powerbank_ID}, {'_id': False}))
     try:
         something = powerbank.pop(0)
@@ -166,7 +177,7 @@ def fee(powerbank_ID: int):
     from_user_database = list(user_database.find({"user_ID": something["user_ID"]}, {'_id': False}))
     user = from_user_database.pop(0)
     current_time = (datetime.now() - timedelta(hours=7)).timestamp() # Current time
-    difference = (current_time - something["end_time"]) # difference / 60 = result in Minute
+    difference = (current_time - something["end_time"]) # Late time in second unit
     fee = (ceil(difference - 30)) * 1 if difference > 30 else 0
     user_database.update_one(user, {"$set": {"user_fee": fee}})
     return {
@@ -180,5 +191,6 @@ def fee(powerbank_ID: int):
 
 @router.get('/get-history')
 def borrow_history():
+    """Get all user that ever borrow powerbank from us."""
     history = list(borrower_history.find({}, {"_id": False}))
     return {"user_history": history}
